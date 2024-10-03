63a4c45cfb78234f8af76772475f44e5b4496d4c
Madan2468 <madanjha2468@gmail.com>
2024-10-04 00:55:37 +0530

    Added animations , effects , and enhancing overal user experience on at header part.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,31 @@
-import "./navbar.css"
-import {Link} from "react-router-dom";
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import "animate.css"; // For built-in animations
+
 function Navbar() {
-    return (
-        <>
-            <header className="navbar-header">
-                <Link to={"/"}>
-                    <h1 className="title">Upchaar Aayog</h1>
-                </Link>
+  return (
+    <>
+      <header className="navbar-header animate__animated animate__fadeInDown animate__delay-0.5s">
+        <Link to="/" className="navbar-brand">
+          <h1 className="title">Upchaar Aayog</h1>
+        </Link>
 
-                <nav className="navbar">
-                    <ul className="navbar-list">
-                        <li><a className="navbar-link" href="/login">Login</a></li>
-                    </ul>
-                </nav>
-            </header>
-            <div className="navbar-backside-frame"></div>
-        </>
-    );
+        <nav className="navbar">
+          <ul className="navbar-list">
+            <li>
+              <a
+                className="navbar-link animate__animated animate__fadeInUp animate__delay-1s"
+                href="/login"
+              >
+                Login
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="navbar-backside-frame"></div>
+    </>
+  );
 }
+
 export default Navbar;
-
-
