bfc8b42c21c3e9fe88c8a193ea237d1ac833934b
ajaynegi45 <ajaynegi3345@icloud.com>
2026-03-31 23:40:56 +0530

    refactor: restructure project into monorepo with separate frontend and backend directories

---
+++ upchaar-aayog-backend/src/test/java/com/upchaar_aayog/UpchaarAayogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upchaar_aayog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpchaarAayogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
