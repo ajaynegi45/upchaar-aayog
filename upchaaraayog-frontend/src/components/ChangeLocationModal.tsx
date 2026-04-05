479485ba0aa1c531ee38297a8a33e012d0d1b3b0
ajaynegi45 <ajaynegi3345@icloud.com>
2026-04-05 15:52:03 +0530

    refactor: remove unused React import from ChangeLocationModal

--- upchaaraayog-frontend/src/components/ChangeLocationModal.tsx
+++ upchaaraayog-frontend/src/components/ChangeLocationModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect} from "react";
+import { useState, useEffect} from "react";
 import { useUserLocationStore } from "@/store/useUserLocationStore";
 import { useLocationStore } from "@/store/useLocationStore";
 
